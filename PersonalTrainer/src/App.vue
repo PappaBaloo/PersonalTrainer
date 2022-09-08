ef5f02273fd0b4b4993e55c412e3ee5d0ca0360e
PappaBaloo <thebettercharlie@gmail.com>
2022-09-08 09:53:38 +0200

    first commit

---
+++ PersonalTrainer/src/App.vue
@@ -0,0 +1,131 @@
+<script setup>
+import TheAccordionVue from "./components/TheAccordion.vue";
+</script>
+
+<template>
+  <header class="all">
+    <nav class="navbar">
+      <h1 class="name_logo">Personal Trainer Finder</h1>
+      <button class="upload" type="button">Upload Profile</button>
+    </nav>
+    <div class="search_filter_profiles">
+      <div class="search_filters">
+        <form action="" method="get" class="form">
+          <input class="searchBar" type="search" placeholder="Search..." />
+          <button type="submit" class="search-button">
+            <img src="./assets/pics/search.png" alt="error" />
+          </button>
+        </form>
+        <the-accordion-vue class="accordion">
+          <template v-slot:title>
+            <span>Filters</span>
+          </template>
+          <template v-slot:content>
+            <p>tjena</p>
+          </template>
+        </the-accordion-vue>
+      </div>
+      <div class="profiles"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    TheAccordionVue,
+  },
+};
+</script>
+
+<style scoped>
+.all {
+  display: grid;
+  grid-template-rows: 0.4fr 1.6fr;
+  width: 100%;
+  height: 100%;
+  margin: 0%;
+}
+
+.navbar {
+  display: flex;
+  flex-wrap: nowrap;
+  justify-content: center;
+  border-bottom: 2px solid gray;
+}
+
+.name_logo {
+  float: left;
+}
+
+.upload {
+  height: 50px;
+  width: 100px;
+  margin-left: auto;
+  margin-top: 6px;
+  padding: 1%;
+  background-color: #3662ae;
+  color: azure;
+  border: 1px solid black;
+  border-radius: 8px;
+  transition-duration: 0.4s;
+}
+
+.upload:hover {
+  background-color: #14233c;
+}
+
+.search_filter_profiles {
+  display: grid;
+  place-items: center;
+  grid-template-columns: 0.6fr 1.4fr;
+  grid-template-rows: 1fr;
+  grid-column: 1;
+  grid-row: 2;
+  margin: 2%;
+}
+
+.search_filters {
+  grid-column: 1;
+  display: inline-flex;
+  flex-direction: column;
+  margin: 1%;
+}
+
+.form {
+  display: flex;
+  flex-direction: row;
+}
+
+.searchBar {
+  border: 2px solid black;
+  padding: 2%;
+  border-radius: 100px;
+  width: 100%;
+}
+
+.search-button {
+  background: transparent;
+  border: none;
+  outline: none;
+  margin-left: -33px;
+}
+.search-button img {
+  width: 20px;
+  height: 20px;
+  object-fit: cover;
+}
+
+.search-button img:hover {
+  cursor: pointer;
+}
+
+.accordion {
+  margin-top: 20px;
+}
+
+.profiles {
+  grid-column: 2;
+}
+</style>
---
+++ PersonalTrainer/src/components/TheAccordion.vue
@@ -0,0 +1,67 @@
+<script setup></script>
+
+<template>
+  <div class="entireAccordion">
+    <button
+      @click="toggleAccordion()"
+      class="button"
+      :aria-expanded="isOpen"
+      :aria-controls="`collapse${_uid}`"
+    >
+      <slot name="title" />
+      <img
+        src="../assets/pics/upArrow.png"
+        alt="error"
+        @click="toggleImgRotate"
+        v-bind:style="{ transform: `rotate(${deg}deg)` }"
+      />
+    </button>
+
+    <div v-show="isOpen" :id="`collapse${_uid}`">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.entireAccordion {
+  width: 250px;
+  height: 100px;
+}
+.button {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  background: none;
+  border: 2px solid black;
+  width: 250px;
+  transition-duration: 0.3s;
+}
+
+.button:hover {
+  background-color: gray;
+}
+img {
+  width: 25px;
+  height: 30px;
+  transform: rotate(180deg);
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+
+  methods: {
+    toggleAccordion() {
+      this.isOpen = !this.isOpen;
+      this.deg += 180;
+    },
+  },
+};
+</script>
